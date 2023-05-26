6f92cdd7974b1597551510a38b181c5f2c11d131
Pawel Plociennik <p.plociennik@wp.pl>
2023-05-26 11:46:59 +0200

    Stworzono druga klase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 class Uczen:
-    def __init__(self, imie_ucznia, nazwisko_ucznia, nazwa_klasy, lekcje):
+    def __init__(self, nazwisko_ucznia, imie_ucznia, nazwa_klasy, lekcje):
         self.nazwisko_ucznia = nazwisko_ucznia
         self.imie_ucznia = imie_ucznia
         self.nazwa_klasy = nazwa_klasy
@@ -10,12 +10,21 @@ class Uczen:
         imie_ucznia = input("Podaj imie ucznia:   ")
         nazwa_klasy = input("Podaj nazwe klasy:   ")
         lekcje = input("Podaj nazwe lekcji ucznia:   ")
-        print(nazwisko_ucznia, imie_ucznia, nazwa_klasy, lekcje)
         lista_uczniow[nazwisko_ucznia] = {"imie": imie_ucznia, "nazwa_klasy": nazwa_klasy, "lekcje": lekcje}
 
+
 class Nauczyciel:
-    def __init__(self):
-        pass
+    def __init__(self, nazwisko_nauczyciela, imie_nauczyciela, przedmiot):
+        self.nazwisko_nauczyciela = nazwisko_nauczyciela
+        self.imie_nauczyciela = imie_nauczyciela
+        self.przedmiot = przedmiot
+
+    def dodawanie_nauczyciela_do_listy (self):
+        nazwisko_nauczyciela = input("Podaj nazwisko nauczyciela:   ")
+        imie_nauczyciela = input("Podaj imie nauczyciela:    ")
+        przedmiot = input("Podaj przedmiot, którego nauczyciel uczy:    ")
+        lista_nauczycieli[nazwisko_nauczyciela] = {"imie": imie_nauczyciela, "przedmiot": przedmiot}
+
 class Wychowawca:
     def __init__(self):
         pass
@@ -24,17 +33,18 @@ class Klasa:
         pass
 
 lista_uczniow = dict()
-
-# lista_nauczycieli = [{"imie": "Zbigniew", "nazwisko": "Wisniewski", "lekcje": "matematyka"}]
+lista_nauczycieli = dict()
 # lista_wychowawcow = [{"imie": "Ryszard", "nazwisko": "Pawlak", "nazwa_klasy": "1C"}]
 # lista_klas = ["1A", "1B", "2A", "2B", "3A", "3C", "4A", "4B"]
 
 nowy_uczen = Uczen("nazwisko_ucznia", "imie_ucznia", "nazwa_klasy", "lekcje")
-
 nowy_uczen.dodawanie_ucznia_do_listy()
 
+nowy_nauczyciel = Nauczyciel("nazwisko_nauczyciela", "imie_nauczyciela", "przedmiot")
+nowy_nauczyciel.dodawanie_nauczyciela_do_listy()
 
 print(lista_uczniow)
+print(lista_nauczycieli)
 
 while True:
     pass
